2b858dfde0930e2042e06e5ecb2c44e5f3f3210c
Raina Shin <yewon95@vt.edu>
2020-01-07 18:26:11 -0500

    updated object drill 4

--- objectDrills4.js
+++ objectDrills4.js
@@ -1,9 +1,13 @@
+/* eslint-disable strict */
 const array = [
-    {Sarah: 'engineer'},
-    {Elise: 'developer'},
-    {Tom: 'boss'}
-  ];
+  {name: 'Sarah',
+    jobTitle: 'engineer'},
+  {name: 'Elise',
+    jobTitle: 'developer'},
+  {name: 'Tom',
+    jobTitle: 'boss'}
+];
   
-  array.forEach(obj => console.log(obj));
+array.forEach(obj => console.log(`${obj.name}: ${obj.jobTitle}`));
   
   
